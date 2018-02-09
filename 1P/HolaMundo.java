8f71acecb6d5ac33ac534c0aa5b038cf484f2c43
lletfrix <lletfrix@gmail.com>
2018-02-09 17:39:54 +0100

    1P-Ex1-Ex2

--- 1P/HolaMundo.java
+++ 1P/HolaMundo.java
@@ -1,17 +1,17 @@
 /**
- * Esta aplicación muestra el mensaje "Hola mundo!" por pantalla
+ * Esta aplicacion muestra el mensaje "Hola mundo!" por pantalla
  *
- * @author Estudiante EPS estudiante.eps@uam.es
+ * @author Rafael Sánchez Sánchez rafael.sanchezs@estudiante.uam.es
  *
-*/
+ */
 public class HolaMundo {
- 
+
  /**
-  * Punto de entrada a la aplicación.
-  * 
-  * Este método imprime el mensaje "Hola mundo!"
+  * Punto de entrada a la aplicacion.
   *
-  * @param args Los argumentos de la línea de comando
+  * Este metodo imprime el mensaje "Hola mundo!"
+  *
+  * @param args Los argumentos de la linea de comando
   */
    public static void main(String[] args) {
       System.out.println("Hola mundo!");   // muestra el string por stdout
