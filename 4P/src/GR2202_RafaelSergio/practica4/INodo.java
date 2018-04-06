4d1579f68f645f351cbdf0a924c56ff8681e1c44
lletfrix <lletfrix@gmail.com>
2018-04-06 17:41:19 +0200

    Soy Programador

--- 4P/src/GR2202_RafaelSergio/practica4/INodo.java
+++ 4P/src/GR2202_RafaelSergio/practica4/INodo.java
@@ -1,4 +1,11 @@
 package GR2202_RafaelSergio.practica4;
 
+import java.util.List;
+
 public interface INodo {
+    public String getRaiz();
+    public List<INodo> getDescendientes();
+    public void incluirDescendiente(INodo nodo);
+    public double calcular();
+    public INodo copy();
 }
--- 4P/src/GR2202_RafaelSergio/practica4/Nodo.java
+++ 4P/src/GR2202_RafaelSergio/practica4/Nodo.java
@@ -1,4 +1,32 @@
 package GR2202_RafaelSergio.practica4;
 
+import java.util.List;
+
 public abstract class Nodo {
+    private String simbolo;
+    private List<INodo> descendientes;
+    private int maxDesc;
+
+    public String getRaiz() {
+        return simbolo;
+    }
+    public List<INodo> getDescendientes() {
+        return descendientes;
+    }
+    public void incluirDescendiente(INodo nodo) {
+        if (this.descendientes.size() >= this.maxDesc) {
+            //Exception
+            return;
+        }
+        this.descendientes.add(nodo);
+        this.maxDesc = this.maxDesc + 1;
+        return;
+    }
+    public abstract double calcular();
+    public INodo copy() throws CloneNotSupportedException {
+        INodo n;
+        n = (INodo) this.clone();
+        return n;
+    }
+
 }
